af4b6da6537ce213489b905d3ff49d35a3eac811
Yuliy Sozin <j.batkovich@gmail.com>
2020-05-24 14:43:28 -0700

    Button anim and white line fixes

--- src/components/BackToTopBtn.js
+++ src/components/BackToTopBtn.js
@@ -1,27 +1,51 @@
-import React from "react"
+import React, { Component } from "react"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faAngleUp } from "@fortawesome/free-solid-svg-icons"
+import Rotate from "react-reveal/Rotate"
 
-const BackToTopBtn = () => {
-  const scrollToTop = () => {
+class BackToTopBtn extends Component {
+  state = { hasScrolled: false }
+
+  componentDidMount() {
+    document.addEventListener("scroll", this.onScroll)
+  }
+
+  onScroll = () => {
+    const c = document.documentElement.scrollTop || document.body.scrollTop
+    if (c > 200 && !this.state.hasScrolled) {
+      this.setState({ hasScrolled: true })
+    } else if (c < 100 && this.state.hasScrolled) {
+      this.setState({ hasScrolled: false })
+    }
+  }
+
+  scrollToTop = () => {
     const c = document.documentElement.scrollTop || document.body.scrollTop
     if (c > 0) {
-      window.requestAnimationFrame(scrollToTop)
+      window.requestAnimationFrame(this.scrollToTop)
       window.scrollTo(0, c - c / 8)
     }
   }
 
-  return (
-    <button
-      href="#top"
-      className="back-to-top"
-      target="_blank"
-      rel="noreferrer"
-      onClick={scrollToTop}
-    >
-      <FontAwesomeIcon icon={faAngleUp} />
-    </button>
-  )
+  render() {
+    return (
+      <>
+        {this.state.hasScrolled && (
+          <Rotate bottom right>
+            <button
+              href="#top"
+              className="back-to-top"
+              target="_blank"
+              rel="noreferrer"
+              onClick={this.scrollToTop}
+            >
+              <FontAwesomeIcon icon={faAngleUp} />
+            </button>
+          </Rotate>
+        )}
+      </>
+    )
+  }
 }
 
 export default BackToTopBtn
